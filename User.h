30b65c8be5e767f6b6e6fa7d060633aced268a5e
Alexa Yaw <50901322+Alexayaw@users.noreply.github.com>
2022-06-27 13:53:52 +0800

    Add files via upload
    Created User linkedlist object and login function

---
+++ User.h
@@ -0,0 +1,33 @@
+
+#pragma once
+#ifndef USER
+#define USER
+#include <iostream> 
+#include <fstream> 
+#include <string>
+using namespace std;
+
+class User {
+private:
+	string full_name;
+	string username;
+	string password;
+	string role;
+public:
+	User* next;
+
+	string get_user_full_name(void) { return full_name; }
+	string get_username(void) { return username; }
+	string get_password(void) { return password; }
+	string get_role(void) { return role; }
+
+	void set_user_full_name(string name) { full_name = name; }
+	void set_username(string usrname) { username = usrname; }
+	void set_password(string pass) { password = pass; }
+	void set_role(string usrrole) { role = usrrole; }
+	
+};
+
+
+
+#endif
