91fe894eb9da5326a5f44e2a7383210f51892cb3
fangyung <fangyung@mailer.blueoceanit.com.my>
2022-07-06 13:39:11 +0800

    Added Queue Attributes Methods

--- OrderUpdateQueue.h
+++ OrderUpdateQueue.h
@@ -1,15 +1,28 @@
-/* Header FIle for the Order Update Queue Class (Use Arrays) */
+/* Header File for the Order Update Queue Class (Use Arrays) */
 
 #pragma once
 #ifndef ORDERUPDATEQUEUE
 #define ORDERUPDATEQUEUE
 
 #include <iostream>
+#include "PurchaseRecords.h"
 using namespace std;
 
 class Order_Update_Queue {
-private:
-public:
+	private:
+		int size;
+		Purchase_Records* linked_list;
+		Purchase_Records* queue;
+
+	public:
+		Order_Update_Queue(int size, Purchase_Records* head);
+		void modify_order_queue();
 };
+
+Order_Update_Queue::Order_Update_Queue(int size, Purchase_Records* head) {
+	this->size = size;
+	this->linked_list = head;
+	this->queue = new Purchase_Records[size];		// create array with fixed size
+}
 
 #endif
