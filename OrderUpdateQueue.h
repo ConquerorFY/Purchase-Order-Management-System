4269229ba21b14a56322cc1125d9943b8cd31f23
fangyung <fangyung@mailer.blueoceanit.com.my>
2022-07-11 13:40:19 +0800

    Modify Queue Methods Header

--- OrderUpdateQueue.h
+++ OrderUpdateQueue.h
@@ -11,12 +11,16 @@ using namespace std;
 class Order_Update_Queue {
 	private:
 		int size;
+		int index;
 		Purchase_Records* linked_list;
 		Purchase_Records* queue;
 
 	public:
 		Order_Update_Queue(int size, Purchase_Records* head);
 		void modify_order_queue();
+		void enqueue(Purchase_Records* node);
+		void dequeue();
+		void display();
 };
 
 Order_Update_Queue::Order_Update_Queue(int size, Purchase_Records* head) {
